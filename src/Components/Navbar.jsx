dde9f5f5f2d3f5f07793a977f63b18b27a7f5d4f
Ayoola Olaoye <communicatti@gmail.com>
2023-07-05 18:01:05 +0100

    further improvements trying to overlay

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
         <div className='nav-links md:flex md:space-x-8 md:items-center md:opacity-100 opacity-0 transition-all ease-in duration-200'>
             <ul className='nav--links flex md:space-x-8 z-[-1] md:z-auto md:static'>
                 <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>01.</span><a href='#'>About</a></li>
-                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>02.</span><a href='#'>About</a></li>
-                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>03.</span><a href='#'>About</a></li>
-                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>04.</span><a href='#'>About</a></li>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>02.</span><a href='#'>Experience</a></li>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>03.</span><a href='#'>Work</a></li>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-300'>04.</span><a href='#'>Contact</a></li>
             </ul>
             <button type='button' className='border border-green-300 p-2 rounded hover:bg-slate-950 md:mt-0'>Resume</button>
         </div>
--- src/Pages/MyProjects.jsx
+++ src/Pages/MyProjects.jsx
@@ -14,15 +14,17 @@ const MyProjects = () => {
 
         <div className='md:relative'>
             <div className='md:flex w-[100%] hidden'>
-                <img src={Xeleron} alt='Xeleron' className='basis-1/2 w-[50%]' />
+                <div className='bg-slate-700 rounded-md'>
+                    <img src={Xeleron} alt='Xeleron' className='basis-1/2 w-[100%] h-[100%] object-cover mix-blend-overlay hover:mix-blend-normal rounded-md' />
+                </div>
+                
                 <div className='basis-1/2  text-right flex flex-col justify-between'>
                     <div>
                         <h1 className='cust--font text-green-300'>Featured Project</h1>
                         <h1 className='md:text-3xl text-lg font-semibold'>Xeleron DApp</h1>
                     </div>
                     <div  >
-                        <ul className='flex cust--font md:text-base text-xs space-x-2 text-green-300 justify-end'>
-                            <li>Blockchain</li>
+                        <ul className='flex cust--font text-xs space-x-2 text-green-300 justify-end'>
                             <li>Solidity</li>
                             <li>Smart Contract</li>
                             <li>React</li>
@@ -32,7 +34,8 @@ const MyProjects = () => {
                 </div>
                 
             </div>
-            <div className='md:w-[55%] bg-slate-800 p-5 rounded md:text-right md:absolute md:top-24 md:left-[45%]'>
+            <div className='md:w-[55%] bg-slate-950 md:bg-slate-800 p-5 rounded md:text-right md:absolute relative md:top-24 md:left-[45%]'>
+                {/* <img src={Xeleron} alt='Xeleron' className='absolute w-full h-full top-0 left-0 md:hidden'/> */}
                 <div>
                     <div className='md:hidden mb-8'>
                         <h1 className='cust--font text-green-300'>Featured Project</h1>
@@ -41,15 +44,14 @@ const MyProjects = () => {
                     <h1 className='mb-8 text-justify'>Xeleron is a groundbreaking <span className='text-green-300'>Decentralized Finance</span> hub that offers diverse range of products such as the ability to pool tokens, swap, farm, stake and launch tokens.
                     </h1>
                     <div className='mb-8'>
-                        <ul className='flex overflow-auto  cust--font md:text-base text-xs space-x-1 text-green-300 md:justify-end md:hidden'>
-                            <li>Blockchain</li>
+                        <ul className='flex  cust--font text-xs space-x-1 text-green-300 md:justify-end md:hidden'>
                             <li>Solidity</li>
                             <li>Smart Contract</li>
                             <li>React</li>
                             <li>TailwindCSS</li>
                         </ul>
                     </div>
-                    <button type='button' className='border p-2 border-green-300 hover:bg-slate-950 duration-200 rounded '>CHECK IT OUT!</button>
+                    <button type='button' className='border p-2 border-green-300 hover:bg-slate-950 duration-200 rounded '>CHECK IT OUT! 👀</button>
 
                 </div>
                 
