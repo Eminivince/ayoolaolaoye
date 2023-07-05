f7080d745b4aeaac2d4d19230ec1b86dca443618
Ayoola Olaoye <communicatti@gmail.com>
2023-07-05 14:41:57 +0100

    Desktop view my project set

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,49 +4,25 @@ import { FaArrowDown } from 'react-icons/fa'
 import { useState } from 'react';
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   return (
     <div className='flex cust--font justify-between lg:mx-10 md:mx-6 pt-10 md:items-center mx-6'>
-      <img src={Logo} alt='logo' className='h-14 hover:-translate-x-1' />
+        <img src={Logo} alt='logo' className='h-14 hover:-translate-x-1' />
 
-      <span className='cursor-pointer md:hidden block' onClick={toggleMenu}>
-        <FaArrowDown />
-      </span>
-      
-      <div
-        className={`nav-links md:flex md:space-x-8 md:items-center ${
-          isMenuOpen ? 'opacity-100' : 'opacity-0'
-        } transition-all ease-in duration-500`}
-      >
-        <ul className='nav--links md:flex md:space-x-8 z-[-1] md:z-auto md:static'>
-          <li className='md:mt-0 mb-2'>
-            <span className='mr-1 text-green-200'>01.</span>
-            <a href='#'>About</a>
-          </li>
-          <li className='md:mt-0 mb-2'>
-            <span className='mr-1 text-green-200'>02.</span>
-            <a href='#'>About</a>
-          </li>
-          <li className='md:mt-0 mb-2'>
-            <span className='mr-1 text-green-200'>03.</span>
-            <a href='#'>About</a>
-          </li>
-          <li className='md:mt-0 mb-2'>
-            <span className='mr-1 text-green-200'>04.</span>
-            <a href='#'>About</a>
-          </li>
-        </ul>
-        <button type='button' className='border border-green-200 p-2 rounded hover:bg-slate-950 md:mt-0'>
-          Resume
-        </button>
-      </div>
+        <span className='cursor-pointer md:hidden block'>
+        </span>
+        <div className='nav-links md:flex md:space-x-8 md:items-center md:opacity-100 opacity-0 transition-all ease-in duration-500'>
+            <ul className='nav--links md:flex md:space-x-8 z-[-1] md:z-auto md:static'>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-200'>01.</span><a href='#'>About</a></li>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-200'>02.</span><a href='#'>About</a></li>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-200'>03.</span><a href='#'>About</a></li>
+                <li className='md:mt-0 mb-2'><span className='mr-1 text-green-200'>04.</span><a href='#'>About</a></li>
+            </ul>
+            <button type='button' className='border border-green-200 p-2 rounded hover:bg-slate-950 md:mt-0'>Resume</button>
+        </div>
+        
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/Pages/MyProjects.jsx
+++ src/Pages/MyProjects.jsx
@@ -4,31 +4,34 @@ import Xeleron from '../Asset/Xeleron.jpeg'
 const MyProjects = () => {
   return (
     <div>
-        <div>
+        <div className='flex space-x-3 items-center my-14'>
             <h1 className='cust--font text-green-200'>03.</h1>
-            <h1>Some Things I've Built</h1>
-            <hr className='w-24' />
+            <h1 className='md:text-3xl text-lg font-semibold'>Some Things I've Built</h1>
+            <hr className='w-[60%]' />
         </div>
 
-        <div>
-            <div>
-                <img src={Xeleron} alt='Xeleron' />
-                <div>
-                    <h1>Featured Project</h1>
-                    <h1>Xeleron DApp</h1>
+        <div className='relative'>
+            <div className='flex w-[100%]'>
+                <img src={Xeleron} alt='Xeleron' className='basis-1/2 w-[50%]' />
+                <div className='basis-1/2  text-right flex flex-col justify-between'>
+                    <div>
+                        <h1 className='cust--font text-green-200'>Featured Project</h1>
+                        <h1 className='md:text-3xl text-lg font-semibold'>Xeleron DApp</h1>
+                    </div>
+                    <div  >
+                        <ul className='flex cust--font md:text-base text-xs space-x-2 text-green-200 justify-end'>
+                            <li>Blockchain</li>
+                            <li>Solidity</li>
+                            <li>Smart Contract</li>
+                            <li>React</li>
+                            <li>TailwindCSS</li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <ul>
-                        <li>Blockchain</li>
-                        <li>Solidity</li>
-                        <li>Smart Contract</li>
-                        <li>React</li>
-                        <li>TailwindCSS</li>
-                    </ul>
-                </div>
+                
             </div>
-            <div>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ratione et tempora natus autem? Soluta iure voluptate repudiandae nemo tempora molestias, minima omnis aperiam et!</h1>
+            <div className='w-[55%] bg-slate-800 p-5 rounded text-right absolute top-24 left-[45%]'>
+                <h1>Xeleron is a groundbreaking Decentralized Finance hub that offers diverse range of products such as the ability to pool tokens, swap, farm, stake and launch tokens.</h1>
             </div>
         </div>
     </div>
